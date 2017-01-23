4cb0498f3ec6ef1677b99bc53cf1be4e824674cb
Kornelia <korneliak.95@gmail.com>
2017-01-23 11:02:24 +0100

    Adding some dependencies

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,9 @@
 /**
  * Created by kornelia on 19.01.17.
  */
+const db = require("./db");
+const Person = require("./../../model/Person");
+const Address = require("./../../model/Address");
+const Product = require("./../../model/Product");
+const Facture = require("./../../model/Facture");
+const Client = require("./../../model/Client");
