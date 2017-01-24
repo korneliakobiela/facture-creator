27569b4891cdf5264ef1dbe8eaf2af01fc1c1bb3
Kornelia <korneliak.95@gmail.com>
2017-01-24 01:38:40 +0100

    Add menu layout and event listeners

--- src/assets/js/bundle.js
+++ src/assets/js/bundle.js
@@ -13237,6 +13237,20 @@ const Product = require("./../../model/Product");
 const Facture = require("./../../model/Facture");
 const Client = require("./../../model/Client");
 
+const menuEventHandler = function () {
+    const menu = document.querySelector('#main-menu');
+    menu.addEventListener("click",function (e) {
+        if(e.target !== e.currentTarget){
+            document.querySelector('.active').classList.toggle('active', false);
+            e.target.classList.toggle('active',true);
+            console.log(typeof menu);
+
+        }
+        e.stopPropagation();
+    },false);
+};
+
+menuEventHandler();
 },{"./../../model/Address":15,"./../../model/Client":16,"./../../model/Facture":17,"./../../model/Person":18,"./../../model/Product":19,"./db":13}],15:[function(require,module,exports){
 /**
  * Created by kornelia on 21.01.17.
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -7,3 +7,18 @@ const Address = require("./../../model/Address");
 const Product = require("./../../model/Product");
 const Facture = require("./../../model/Facture");
 const Client = require("./../../model/Client");
+
+const menuEventHandler = function () {
+    const menu = document.querySelector('#main-menu');
+    menu.addEventListener("click",function (e) {
+        if(e.target !== e.currentTarget){
+            document.querySelector('.active').classList.toggle('active', false);
+            e.target.classList.toggle('active',true);
+            console.log(typeof menu);
+
+        }
+        e.stopPropagation();
+    },false);
+};
+
+menuEventHandler();
